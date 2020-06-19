6053be888ac08a6ad657246be8047e243411d846
Ocelot <FinntheRaider@gmail.com>
2020-06-19 00:59:27 -0700

    Update IDamageable.java

--- src/main/java/com/mrcrayfish/guns/interfaces/IDamageable.java
+++ src/main/java/com/mrcrayfish/guns/interfaces/IDamageable.java
@@ -1,5 +1,6 @@
 package com.mrcrayfish.guns.interfaces;
 
+import com.mrcrayfish.guns.common.trace.GunProjectile;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -9,5 +10,5 @@ import net.minecraft.world.World;
  */
 public interface IDamageable
 {
-    void onBlockDamaged(World world, BlockState state, BlockPos pos, int damage);
+    void onBlockDamaged(World world, BlockState state, BlockPos pos, GunProjectile projectile, float rawDamage, int damage);
 }
